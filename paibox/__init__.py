be43c58fe0848b0034695a299e186b534a1df78f
birdswimming <birdswimming3.14@gmail.com>
2025-03-24 18:05:32 +0800

    Preliminary implementation of transpose and concat

--- paibox/__init__.py
+++ paibox/__init__.py
@@ -50,6 +50,9 @@ from .components.neuron.neurons import StoreVoltageNeuron as StoreVoltageNeuron
 # Input projection
 from .components.projection import InputProj as InputProj
 
+from .components.operations import Transpose as Transpose
+from .components.operations import Concat as Concat
+
 # Connection types of synapses
 from .components.synapses import ConnType as SynConnType
 
